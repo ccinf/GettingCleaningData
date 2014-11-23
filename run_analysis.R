362626b9799b595b27adcdf664712e6e70277e98
Chris <chris@webcubicle.com>
2014-11-23 15:27:28 -0500

    Halfway there...

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,19 @@
-function readAndCombineDataSet(dataFile, subjectFile, activityFile) {
+readAndCombineDataSet <- function(dataFile, subjectFile, activityFile, featuresFile) {
     data <- read.table(dataFile)
     subjects <- read.table(subjectFile)
     activities <- read.table(activityFile)
+    features <- read.table(featuresFile)
+    
+    ## Check that our columns match our features
+    numColumns <- ncol(data)
+    numFeatures <- nrow(features)
+    if (!(numColumns == numFeatures)) {
+        message("Number of features and variables do not match")
+        return()
+    }
+    
+    ## Name our columns
+    colnames(data) <- as.vector(features$V2)
     
     ## Check that our sizes match
     dataSize <- nrow(data)
@@ -16,36 +28,51 @@ function readAndCombineDataSet(dataFile, subjectFile, activityFile) {
         return()
     }
     
-    # Combine data and return table 
+    # Combine data, activities, and subjects 
     combined <- cbind(activities, data)
-    cbind(subjects, combined)
-}
-
-function combineDataSets(data1, data2) {
+    combined <- cbind(subjects, combined)
     
-}
-
-function extractMeanAndStd(data) {
+    # Name the first two columns appropriately
+    names <- colnames(combined)
+    names[1] = "subject"
+    names[2] = "activity"
+    colnames(combined) <- names
     
+    # Name the remainder of the columns
+    
+    
+    # Return the data frame
+    combined
 }
 
-function readActivities(labelsFile) {
+combineDataSets <- function(data1, data2) {
+    rbind(data1, data2)
+}
+
+extractMeanAndStd <- function(data) {
+    ## Which columns contain the mean and the std? Use the column names to find
+    ## out which ones to extract. We use a regex to do this.
+    columns <- grep("mean\\(\\)|std\\(\\)",colnames(data))
+    data[columns]
+}
+
+readActivities <- function(labelsFile) {
     read.table(labelsFile)
 }
 
-function readSubjects(subjectsFile) {
+readSubjects <- function(subjectsFile) {
     read.table(subjectsFile)
 }
 
-function mergeActivities(data, labelsData) {
+mergeActivities <- function(data, labelsData) {
     
 }
 
-function mergeSubjects(data, subjectsData) {
+mergeSubjects <- function(data, subjectsData) {
     
 }
 
-function tidyData {
+tidyData <- function() {
     
 }
 
